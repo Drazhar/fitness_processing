a21002dafce79036fb550611e4576e9c9562f4e3
Philip Trauth <trauthp@gmail.com>
2022-03-30 08:56:53 +0200

    Added plotext for rudimentary weight plot

--- src/libra_weight.py
+++ src/libra_weight.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import plotext as plt
 from src.bcolors import bcolors
 
 def import_and_delete(csv_file, db_client):
@@ -9,7 +10,11 @@ def import_and_delete(csv_file, db_client):
   df["date"] = pd.to_datetime(df["date"], yearfirst=True, utc=False)
 
   json_body = []
+  x = []
+  y = []
   for i in range(0, df.shape[0]):
+    x.append((df.iloc[i]["date"] - pd.Timestamp.now()).days)
+    y.append(df.iloc[i]["weight"])
     json_body.append({
       "measurement": "body",
       "time": f"{df.iloc[i]['date'].date()}T12:00:00Z",
@@ -17,6 +22,12 @@ def import_and_delete(csv_file, db_client):
         "weight": df.iloc[i]["weight"],
       }
     })
+
+  plt.plot(x,y)
+  plt.plot_size(80,10)
+  plt.frame(False)
+  plt.colorless()
+  plt.show()
 
   write_success = db_client.write_points(json_body, time_precision='ms')
   print(f"... Successfully written into influxDB: {write_success}")
